e61f864976c47e2625ba3f2392f8db32a270d0ce
Celine2332 <celine.joss@stud.fhgr.ch>
2026-05-08 20:42:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log ('works')
+
+async function startGame(stop) {
+  try {
+    const response = await fetch(
+      `https://tarotapi.dev/api/v1/cards/random`,
+    );
+    if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
+    const data = await response.json();
+    return data.drinks;
+  } catch (error) {
+    console.error("Fetch failed:", error);
+  }
+}
