75200dc66f3102e44d05c65b7172ba7941cb1706
Celine2332 <celine.joss@stud.fhgr.ch>
2026-05-26 12:15:38 +0200

    ergänzung

--- js/script.js
+++ js/script.js
@@ -1,31 +1,22 @@
 const cardContainer = document.getElementById('card-container');
-
 const overlay = document.getElementById('overlay');
-
 const overlayTitle = document.getElementById('overlay-title');
-
 const overlayText = document.getElementById('overlay-text');
 
 
 
-// ─────────────────────
 // API
-// ─────────────────────
 
 async function loadCards(startingNummber) {
 
     const url = 'https://tarotapi.dev/api/v1/cards/random?n=' + startingNummber;
 
     try {
-
         const response = await fetch(url);
-
         const data = await response.json();
-
         return data.cards;
 
     } catch(error) {
-
         console.error(error);
 
     }
@@ -33,9 +24,8 @@ async function loadCards(startingNummber) {
 
 
 
-// ─────────────────────
+
 // Start
-// ─────────────────────
 
 init();
 
@@ -50,30 +40,21 @@ async function init() {
 
 
 
-// ─────────────────────
+
 // Karten erzeugen
-// ─────────────────────
 
 function createCards(cards) {
 
     for (const cardData of cards) {
-
         const shortName = cardData.name_short;
-
         const imagePath = `assets/images/${shortName}.png`;
-
-
-
         const card = document.createElement('div');
 
         card.classList.add('karten');
 
-
-
         card.innerHTML = `
 
             <div class="card">
-
                 <div class="face face-front">
                     <img src="${imagePath}" alt="${cardData.name}">
                 </div>
@@ -81,74 +62,53 @@ function createCards(cards) {
                 <div class="face face-back">
                     <img src="assets/images/back.png" alt="back">
                 </div>
-
             </div>
-
         `;
-
-
 
         cardContainer.appendChild(card);
 
-
-
         const innerCard = card.querySelector('.card');
 
-
-
-        // ─────────────────────
-        // Karten automatisch umdrehen
-        // ─────────────────────
+   
+// Karten automatisch umdrehen
 
         setTimeout(() => {
-
             innerCard.classList.add('flipped');
-
         }, 500);
 
 
-
-        // ─────────────────────
-        // Overlay öffnen
-        // ─────────────────────
+// Overlay öffnen
 
         card.addEventListener('click', () => {
-
             showOverlay(cardData);
-
         });
-
     }
-
 }
 
 
 
-// ─────────────────────
+
 // Overlay anzeigen
-// ─────────────────────
 
 function showOverlay(cardData) {
-
     overlayTitle.textContent = cardData.name;
-
     overlayText.textContent = cardData.meaning_up;
-
     overlay.classList.remove('hidden');
 
 }
 
 
 
-// ─────────────────────
+
 // Overlay schließen
-// ─────────────────────
 
 overlay.addEventListener('click', () => {
 
     overlay.classList.add('hidden');
 
 });
+
+// Kartenwerte
 
 const cardValues = {
   // 1. Major Arcana (Wert 78-60, 19 Karten)
